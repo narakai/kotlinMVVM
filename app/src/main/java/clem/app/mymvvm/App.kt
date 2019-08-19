5b712e540a0c04a33c25cc6883acff51fdd50ca3
clem ray <rayclem89@gmail.com>
2019-08-19 10:16:33 +0800

    add resources

--- app/src/main/java/clem/app/mymvvm/App.kt
+++ app/src/main/java/clem/app/mymvvm/App.kt
@@ -2,14 +2,20 @@ package clem.app.mymvvm
 
 import android.app.Application
 import android.content.Context
+import android.content.res.Configuration
 import android.util.Log
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.multidex.MultiDex
+import androidx.multidex.MultiDexApplication
+import clem.app.mymvvm.ui.MainActivity
+import jonathanfinerty.once.Once
 import kotlin.properties.Delegates
 
 /**
  * Created by leon
  * on 2018/3/13 13:35
  */
-class App : Application() {
+class App : MultiDexApplication() {
 
     companion object {
         var CONTEXT: Context by Delegates.notNull()
@@ -18,5 +24,16 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         CONTEXT = applicationContext
+        Once.initialise(this)
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
+    }
+    //
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+    }
+
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
     }
 }
